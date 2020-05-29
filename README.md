8dbd28b49673e3c5a01acce8c9995caabdbb3ed7
Andrea Vaccari <andrea@andreavaccari.com>
2020-05-29 21:37:13 +0100

    Updates

--- README.md
+++
@@ -1,1 +0,0 @@
-# Clearspace public site
--- gatsby-browser.js
+++
@@ -1,5 +0,0 @@
-import "typeface-inter"
-import "typeface-montserrat"
-import "typeface-montserrat-alternates"
-import "./src/gatsby-plugin-theme-ui/preflight.css"
-
